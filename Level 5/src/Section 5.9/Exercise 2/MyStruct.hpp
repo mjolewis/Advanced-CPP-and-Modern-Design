2a57014d12a63a4e3abc339d81587b19d80dbde1
mlewis <mjolewis@gmail.com>
2023-07-29 14:34:41 -0400

    Boost Signal and Slots 101

---
+++ Level 5/src/Section 5.9/Exercise 2/MyStruct.hpp
@@ -0,0 +1,18 @@
+//
+// Simple Struct to illustrate how to use function objects with signals and slots
+//
+// Created by Michael Lewis on 7/29/23.
+//
+
+#ifndef ADVANCED_CPP_AND_MODERN_DESIGN_MYSTRUCT_HPP
+#define ADVANCED_CPP_AND_MODERN_DESIGN_MYSTRUCT_HPP
+
+struct MyStruct
+{
+    double val;
+    MyStruct(double v);
+    void modify(double newValue);
+};
+
+
+#endif //ADVANCED_CPP_AND_MODERN_DESIGN_MYSTRUCT_HPP
